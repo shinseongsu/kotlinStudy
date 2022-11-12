eb5dd13fd17860e4f33b727fc57e59d062062b47
Shin seongsu <tlstjdtn321@naver.com>
2022-11-12 19:08:46 +0900

    feat: abstract, open final 추가

---
+++ kotlinInAction/src/main/kotlin/book/chapter03/problem03/Animated.kt
@@ -0,0 +1,11 @@
+package book.chapter03.problem03
+
+abstract class Animated {
+
+    abstract fun animate()
+
+    open fun stopAnimating() { }
+
+    fun animateTwice() { }
+
+}
---
+++ kotlinInAction/src/main/kotlin/book/chapter03/problem03/RichButton.kt
@@ -0,0 +1,9 @@
+package book.chapter03.problem03
+
+import book.chapter03.problem01.Clickable
+
+open class RichButton: Clickable {
+
+    final override fun click() {  }
+
+}
