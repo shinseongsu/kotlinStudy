94eb44129bfe9797fe25ed0c7dd7b42a550a9377
Shin seongsu <tlstjdtn321@naver.com>
2022-11-12 19:00:24 +0900

    feat: is 상속 체크

---
+++ kotlinInAction/src/main/kotlin/book/chapter01/problme10/EvalWithLogging.kt
@@ -0,0 +1,20 @@
+package book.chapter01.problme10
+
+import book.chapter01.problme09.Expr
+import book.chapter01.problme09.Num
+import book.chapter01.problme09.Sum
+
+fun evalWithLogging(e: Expr): Int =
+    when (e) {
+        is Num -> {
+            println("num: ${e.value}")
+            e.value
+        }
+        is Sum -> {
+            val left = evalWithLogging(e.left)
+            val right = evalWithLogging(e.right)
+            println("sum: $left + $right")
+            left + right
+        }
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
---
+++ kotlinInAction/src/main/kotlin/book/chapter01/problme10/Main.kt
@@ -0,0 +1,8 @@
+package book.chapter01.problme10
+
+import book.chapter01.problme09.Num
+import book.chapter01.problme09.Sum
+
+fun main() {
+    evalWithLogging( Sum(Sum(Num(1), Num(2)), Num(4)))
+}
