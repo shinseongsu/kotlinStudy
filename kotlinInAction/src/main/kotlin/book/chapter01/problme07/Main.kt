38336dab87e1e39f425783d1d4276c83137703f2
Shin seongsu <tlstjdtn321@naver.com>
2022-11-12 18:59:12 +0900

    feat: enum setOf 사용하기

---
+++ kotlinInAction/src/main/kotlin/book/chapter01/problme07/Main.kt
@@ -0,0 +1,7 @@
+package book.chapter01.problme07
+
+import book.chapter01.problme05.Color.*
+
+fun main() {
+    println(mix(BLUE, YELLOW))
+}
---
+++ kotlinInAction/src/main/kotlin/book/chapter01/problme07/Mix.kt
@@ -0,0 +1,12 @@
+package book.chapter01.problme07
+
+import book.chapter01.problme05.Color
+import book.chapter01.problme05.Color.*
+
+fun mix(c1: Color, c2: Color) =
+    when (setOf(c1, c2)) {
+        setOf(RED, YELLOW) -> ORANGE
+        setOf(YELLOW, BLUE) -> GREEN
+        setOf(BLUE, VIOLET) -> INDIGO
+        else -> throw Exception("Dirth color")
+    }
